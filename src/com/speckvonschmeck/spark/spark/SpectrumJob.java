cea4fe725f095a4304a5cc43fd40ddfde5e6a7eb
Sören Falkenberg <soeren-f@web.de>
2017-06-09 21:01:07 +0200

    corrected merge conflicts

--- src/com/speckvonschmeck/spark/spark/SpectrumJob.java
+++ src/com/speckvonschmeck/spark/spark/SpectrumJob.java
@@ -1,16 +1,14 @@
 package com.speckvonschmeck.spark.spark;
-
-<<<<<<< HEAD
+ 
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.javaFunctions;
-import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapToRow;
 import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapRowTo;
+import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapToRow;
 
-=======
->>>>>>> refs/remotes/origin/kafkaStream
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -32,17 +30,12 @@ import org.apache.spark.streaming.kafka010.OffsetRange;
 import org.slf4j.Logger;
 import org.slf4j.impl.Log4jLoggerFactory;
 
-<<<<<<< HEAD
-import com.datastax.driver.core.Session;
 import com.datastax.spark.connector.cql.CassandraConnector;
 import com.datastax.spark.connector.japi.CassandraJavaUtil;
-import com.datastax.spark.connector.japi.CassandraRow;
-import com.google.gson.Gson;
 import com.speckvonschmeck.spark.cassandra.CassandraTester;
 import com.speckvonschmeck.spark.models.Spectrum;
-=======
+
 import scala.Tuple2;
->>>>>>> refs/remotes/origin/kafkaStream
 
 public class SpectrumJob {
 	
@@ -51,10 +44,7 @@ public class SpectrumJob {
 			System.getenv("KAFKA_URL")
 			: "192.168.178.64:9092";//64
 			
-<<<<<<< HEAD
 	public final static String CASSANDRA_URL = "localhost";//64
-=======
->>>>>>> refs/remotes/origin/kafkaStream
 			
 	public final static String KAFKA_TOPIC = System.getenv("KAFKA_TOPIC") != null ? 
 			System.getenv("KAFKA_TOPIC")
@@ -68,8 +58,7 @@ public class SpectrumJob {
 		
 		JavaSparkContext sc = new JavaSparkContext(conf);
 		JavaStreamingContext context = new JavaStreamingContext(sc, new Duration(2000));
-		
-<<<<<<< HEAD
+		 
 		
         CassandraConnector connector = CassandraConnector.apply(conf);
         
@@ -89,9 +78,7 @@ public class SpectrumJob {
                 .cassandraTable("alpha", "spectrum", mapRowTo(Spectrum.class));
 		javaFunctions(personRdd).writerBuilder("beta", "spectrum", mapToRow(Spectrum.class)).saveToCassandra();	
 		
-        
-=======
->>>>>>> refs/remotes/origin/kafkaStream
+         
         Map<String, Object> kafkaParams = new HashMap<>();
 		kafkaParams.put("bootstrap.servers", KAFKA_URL);
 		kafkaParams.put("key.deserializer", StringDeserializer.class);
@@ -101,70 +88,7 @@ public class SpectrumJob {
 		kafkaParams.put("enable.auto.commit", false);
 
 		Collection<String> topics = Arrays.asList(KAFKA_TOPIC);
-
-<<<<<<< HEAD
-//		final JavaInputDStream<ConsumerRecord<String, String>> dstream = KafkaUtils.createDirectStream(context,
-//				LocationStrategies.PreferConsistent(),
-//				ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams));
-//		
-//	    dstream.map(new Function<ConsumerRecord<String, String>, Spectrum>() {
-//
-//			private static final long serialVersionUID = 1L;
-//
-//			@Override
-//			public Spectrum call(ConsumerRecord<String, String> record) throws Exception {
-//				log.warn("-----------HALLLOOOOOOOOOO----------");
-//				return new Gson().fromJson(record.value(), Spectrum.class);
-//			}
-//
-//		}).foreachRDD(new VoidFunction<JavaRDD<Spectrum>>(){
-//
-//			private static final long serialVersionUID = 1L;
-//
-//			@Override
-//			public void call(JavaRDD<Spectrum> rdd) throws Exception {
-//				// TODO Auto-generated method stub
-//				log.warn("-----------HALLLOOOOOOOOOO----------");
-//				if(rdd!=null){
-//					log.warn(rdd.toDebugString());
-//					log.warn(String.valueOf(rdd.count()));
-//			        //javaFunctions(rdd).writerBuilder("alpha", "spectrum", mapToRow(Spectrum.class)).saveToCassandra();			
-//					rdd.foreachAsync(new VoidFunction<Spectrum>() {
-//
-//						private static final long serialVersionUID = 1L;
-//
-//						@Override
-//						public void call(Spectrum spectrum) throws Exception {
-//							// TODO Auto-generated method stub
-//					        
-//					        List<Spectrum> spectra = CassandraTester.generateSpectraFromSpectrum(spectrum);
-//							
-//							JavaRDD<Spectrum> rdd2 = (sc).parallelize(spectra);
-//							javaFunctions(rdd2).writerBuilder("alpha", "spectrum", mapToRow(Spectrum.class)).saveToCassandra();							
-//					        
-//							
-//							log.warn("-----------HALLLOOOOOOOOOO----------");
-//							log.warn(spectrum.toString());
-//							
-//							
-//						}
-//					});
-//				}
-//				
-//			}
-//	    	
-//	    });
-//        
-//	    
-//	    
-//		context.start();
-//		try {
-//			context.awaitTermination();
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-=======
+ 
 		final JavaInputDStream<ConsumerRecord<String, String>> dstream = KafkaUtils.createDirectStream(context,
 				LocationStrategies.PreferConsistent(),
 				ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams));
@@ -205,59 +129,7 @@ public class SpectrumJob {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
->>>>>>> refs/remotes/origin/kafkaStream
+		} 
 	}	
 
 }
-//
-//
-///*
-// * noch nicht getestet: treffen mit roman am 09.06.17
-// */
-//JavaDStream<String> stream1 = dstream.transform(
-//	      // Make sure you can get offset ranges from the rdd
-//	      new Function<JavaRDD<ConsumerRecord<String, String>>,
-//	        JavaRDD<ConsumerRecord<String, String>>>() {
-//	          @Override
-//	          public JavaRDD<ConsumerRecord<String, String>> call(
-//	            JavaRDD<ConsumerRecord<String, String>> rdd
-//	          ) {
-//	            OffsetRange[] offsets = ((HasOffsetRanges) rdd.rdd()).offsetRanges();
-//	            offsetRanges.set(offsets);
-//	            return rdd;
-//	          }
-//	        }
-//	    ).map(
-//	        new Function<ConsumerRecord<String, String>, String>() {
-//	          @Override
-//	          public String call(ConsumerRecord<String, String> r) {
-//	        	  log.warn("HEY");
-//	        	  log.warn(r.value());
-//	            return r.value();
-//	          }
-//	        }
-//	    );
-//stream1.foreachRDD(new VoidFunction<JavaRDD<String>>() {
-//
-//	@Override
-//	public void call(JavaRDD<String> rdd) throws Exception {
-//		// TODO Auto-generated method stub
-//		log.warn("HEYHEYHEY");
-//		if(rdd!=null){
-//			log.warn(rdd.toDebugString());
-//			log.warn(String.valueOf(rdd.count()));
-//	        //javaFunctions(rdd).writerBuilder("alpha", "spectrum", mapToRow(Spectrum.class)).saveToCassandra();			
-//			rdd.foreachAsync(new VoidFunction<String>() {
-//
-//				@Override
-//				public void call(String t) throws Exception {
-//					// TODO Auto-generated method stub
-//					log.warn("HIER KOMMT T");
-//					log.warn(t);
-//				}
-//				
-//			});
-//		}
-//	}
-//});
